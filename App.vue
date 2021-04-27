fd06808abdbb655dc84bd7c50f55ee7b2e565821
jasmynej14 <jasmynej14@brandeis.edu>
2021-04-26 22:53:55 -0400

    attempting video component

--- App.vue
+++ App.vue
@@ -1,17 +1,38 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+
+    <text>Title</text>
+    
+    <Feed/>
+    
   </view>
 </template>
 
+<script>
+import Feed from "./components/Feed";
+export default {
+  components: { Feed },
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    }
+  },
+};
+</script>
+
 <style>
 .container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
   flex: 1;
+  background-color: white;
+  margin-top: 70px;
 }
 .text-color-primary {
   color: blue;
+  font-size: 30;
 }
 </style>
---
+++ components/Feed.vue
@@ -0,0 +1,37 @@
+<template>
+   <scroll-view class="feed">
+        <text>Feed</text>
+        <feed-item v-for="user in users" :key="user.name" :user="user" class="feed-item"/>
+   </scroll-view> 
+</template>
+
+<script>
+import FeedItem from './FeedItem'
+
+export default {
+    components: { FeedItem },
+    data(){
+        return{
+           users: [
+               {name:"Jasmyne",content:"Video"},
+               {name:"Jasmyne1",content:"Video"},
+               {name:"Jasmyne2",content:"Video"},
+               {name:"Jasmyne3",content:"Video"},
+               {name:"Jasmyne4",content:"Video"}
+           ] 
+        }
+    }
+}
+</script>
+
+<style>
+
+    .feed{
+        background-color: beige;
+        padding: 50px;
+    }
+
+    .feed-item{
+        margin-bottom: 20px;
+    }
+</style>
